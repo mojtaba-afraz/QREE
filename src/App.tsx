e7f9583c3dc1c82c88ded10278d69651043fbd70
mojtaba afraz <mojtabaafraz1@gmail.com>
2022-08-31 03:19:50 +0430

    Install MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Grid from '@mui/material/Grid'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Grid container spacing={2}>
+            <Grid item xs={8}>
+                xs=8
+            </Grid>
+            <Grid item xs={4}>
+                xs=4
+            </Grid>
+            <Grid item xs={4}>
+                xs=4
+            </Grid>
+            <Grid item xs={8}>
+                xs=8
+            </Grid>
+        </Grid>
     </div>
   )
 }
