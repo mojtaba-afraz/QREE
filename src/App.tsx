8d17b9a4174cfb1d87d97dc68936bf606299670a
mojtaba afraz <mojtabaafraz1@gmail.com>
2022-08-31 04:50:50 +0430

    add QR generator card

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,74 @@
-import { useState } from 'react'
-import './App.css'
+import {SetStateAction, useState} from 'react'
+import QRCode from 'react-qr-code';
 import Grid from '@mui/material/Grid'
+import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import {Button, colors} from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+    let inputValue = ''
+    const [QrValue, setQrValue] = useState('')
+    const updateQR = (NewValue:SetStateAction<string>):void => {
+    UpdateInputValue('')
+    setQrValue(NewValue)
+    };
+    const UpdateInputValue = (newValue:string):void=>{
+        inputValue = newValue
+    }
 
-  return (
-    <div className="App">
-        <Grid container spacing={2}>
-            <Grid item xs={8}>
-                xs=8
-            </Grid>
-            <Grid item xs={4}>
-                xs=4
-            </Grid>
-            <Grid item xs={4}>
-                xs=4
-            </Grid>
-            <Grid item xs={8}>
-                xs=8
-            </Grid>
-        </Grid>
-    </div>
-  )
+    return (
+        <div className="App">
+            <Container maxWidth="md">
+                <Grid container spacing={2}>
+                    <Grid item md={12} xs={12} xl={12}>
+                            <Card sx={{p: 5,bgcolor: "#9d9d9d",borderRadius:10}} variant="outlined">
+                                    <Grid item md={12} xs={12} xl={12}>
+                                        <Box sx={{
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            width:'100%'
+                                        }}>
+                                            <Card sx={{p: 5,borderRadius:10,mb:10,width:'256px',height:'256px'}}>
+                                                {QrValue &&
+                                                    <QRCode value={QrValue}/>
+                                                }
+                                            </Card>
+                                        </Box>
+                                    </Grid>
+                                    <Grid item md={12} xs={12} xl={12}>
+                                        <Box sx={{
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            width:'100%'
+                                        }}>
+                                            {inputValue}
+                                        <TextField
+                                            onChange={e => UpdateInputValue(e.target.value)}
+                                            label="Text You Want To Make QR"
+                                            defaultValue={inputValue}
+                                            sx={{width:'100%'}}
+                                            color={'primary'}
+                                        />
+                                        </Box>
+                                        <Box sx={{
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            width:'100%',
+                                            margin:'20px 0'
+                                        }}>
+                                            <Button sx={{width:'100%'}} onClick={ e => updateQR(inputValue)} color={"warning"} variant="contained">
+                                                generate
+                                            </Button>
+                                        </Box>
+                                    </Grid>
+                            </Card>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    )
 }
 
 export default App
